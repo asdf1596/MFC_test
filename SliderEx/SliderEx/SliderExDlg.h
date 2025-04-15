bc19e98e9ba62ea2fb64d0f7eded57b7043f4f64
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2025-04-15 19:01:09 +0900

    20250415_2

---
+++ SliderEx/SliderEx/SliderExDlg.h
@@ -0,0 +1,40 @@
+﻿
+// SliderExDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CSliderExDlg 대화 상자
+class CSliderExDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CSliderExDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SLIDEREX_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CSliderCtrl m_hor;
+	CSliderCtrl m_ver;
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	int m_nVer;
+	int m_nHor;
+};
