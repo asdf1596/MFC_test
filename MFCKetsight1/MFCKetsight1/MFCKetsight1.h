0724637970d107796108f9130c991281411c8c62
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2025-05-13 10:58:40 +0900

    250513_1058

---
+++ MFCKetsight1/MFCKetsight1/MFCKetsight1.h
@@ -0,0 +1,32 @@
+﻿
+// MFCKetsight1.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CMFCKetsight1App:
+// 이 클래스의 구현에 대해서는 MFCKetsight1.cpp을(를) 참조하세요.
+//
+
+class CMFCKetsight1App : public CWinApp
+{
+public:
+	CMFCKetsight1App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCKetsight1App theApp;
---
+++ MFCKetsight1/MFCKetsight1/MFCKetsight1Dlg.h
@@ -0,0 +1,41 @@
+﻿
+// MFCKetsight1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCKetsight1Dlg 대화 상자
+class CMFCKetsight1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCKetsight1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCKETSIGHT1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int e_for;
+	int e_max;
+	int e_min;
+	CComboBox m_comboDevice;
+	afx_msg void OnBnClickedButtonOpen();
+	afx_msg void OnClickedButtonScan();
+};
